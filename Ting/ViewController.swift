e56cbd879c56550d8e0a520f719d2368e883383d
Quaker-Lee <tkacl2010@gmail.com>
2025-01-20 23:45:18 +0900

    Feat: 폰트 추가, Hex Color 변환 구현

--- Ting/ViewController.swift
+++ Ting/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureUI()
     }
+    
+    func configureUI() {
+        
+        view.backgroundColor = UIColor(hexCode: "FFF7ED")
+        let testLabel = UILabel().then {
+            $0.text = "TING"
+            $0.font = UIFont(name: "Gemini Moon", size: 50)
+            $0.textColor = UIColor(hexCode: "C2410C")
+        }
+        
+        view.addSubview(testLabel)
+        testLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(15)
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+        }
+    }
+    
+    
 
 
 }
 
+//RGB를 Hex Color로 변환
+extension UIColor {
+    
+    convenience init(hexCode: String, alpha: CGFloat = 1.0) {
+        var hexFormatted: String = hexCode.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+        
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
+        }
+        
+        assert(hexFormatted.count == 6, "Invalid hex code used.")
+        
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+        
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                  alpha: alpha)
+    }
+}
