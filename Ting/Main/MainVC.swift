c844f5c7c6800770e2ac2a2a3e591cb8dfdb20f6
Quaker-Lee <tkacl2010@gmail.com>
2025-01-21 13:58:38 +0900

    Refactor: 메인VC 이름 수정, 폴더 정리

--- Ting/Main/MainVC.swift
+++ Ting/Main/MainVC.swift
@@ -9,36 +9,40 @@ import UIKit
 import SnapKit
 import Then
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
+    // MARK: UI요소들
+    let logo = UILabel().then {
+        $0.text = "TING"
+        $0.font = UIFont(name: "Gemini Moon", size: 50)
+        $0.textColor = UIColor(hexCode: "C2410C")
+    }
+//    let searchBar = UISearchBar().then {
+//        $0.
+//    }
+    
+    // MARK: viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureUI()
     }
     
+    // MARK: UI 구성
     func configureUI() {
-        
         view.backgroundColor = UIColor(hexCode: "FFF7ED")
-        let testLabel = UILabel().then {
-            $0.text = "TING"
-            $0.font = UIFont(name: "Gemini Moon", size: 50)
-            $0.textColor = UIColor(hexCode: "C2410C")
-        }
         
-        view.addSubview(testLabel)
-        testLabel.snp.makeConstraints {
+        view.addSubview(logo)
+        logo.snp.makeConstraints {
             $0.leading.equalToSuperview().offset(15)
             $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
         }
     }
     
-    
-
-
 }
 
-//RGB를 Hex Color로 변환
+// MARK: extensions
+// RGB를 Hex Color로 변환
 extension UIColor {
     
     convenience init(hexCode: String, alpha: CGFloat = 1.0) {
