3b619c1507b034a756023abd29b1d893c43c8220
Quaker-Lee <tkacl2010@gmail.com>
2025-01-27 02:20:22 +0900

    Feat: TextFiled 색 변경, 저장 버튼 액션 추가

--- Ting/MyPage/EditInfoVC.swift
+++ Ting/MyPage/EditInfoVC.swift
@@ -15,7 +15,7 @@ class EditInfoVC: UIViewController {
     private let titleLabel = UILabel().then {
         $0.text = "회원정보 수정"
         $0.textColor = .brownText
-        $0.font = .boldSystemFont(ofSize: 20)
+        $0.font = .boldSystemFont(ofSize: 30)
         $0.textAlignment = .left
     }
     
@@ -30,18 +30,22 @@ class EditInfoVC: UIViewController {
         $0.distribution = .fillEqually
     }
     
-    private let nameField = EditCustomView(labelText: "이름", placeholder: "이름을 입력하세요")
-    private let skillStackField = EditCustomView(labelText: "기술 스택", placeholder: "예: Swift, Kotlin")
-    private let toolField = EditCustomView(labelText: "사용 툴", placeholder: "예: Xcode, Android Studio")
-    private let workStyleField = EditCustomView(labelText: "협업 방식", placeholder: "예: 온라인, 오프라인, 무관")
-    private let locationField = EditCustomView(labelText: "지역", placeholder: "거주 지역을 입력하세요")
-    private let interestField = EditCustomView(labelText: "관심사", placeholder: "관심 있는 분야를 입력하세요")
+    private let nameField = EditCustomView(labelText: "이름", placeholder: "  이름을 입력하세요")
+    private let skillStackField = EditCustomView(labelText: "기술 스택", placeholder: "  예: Swift, Kotlin")
+    private let toolField = EditCustomView(labelText: "사용 툴", placeholder: "  예: Xcode, Android Studio")
+    private let workStyleField = EditCustomView(labelText: "협업 방식", placeholder: "  예: 온라인, 오프라인, 무관")
+    private let locationField = EditCustomView(labelText: "지역", placeholder: "  거주 지역을 입력하세요")
+    private let interestField = EditCustomView(labelText: "관심사", placeholder: "  관심 있는 분야를 입력하세요")
     
     
-    private let saveButton = UIButton().then {
-        $0.setTitle("저장", for: .normal)
-        $0.backgroundColor = .brownText
+    private let saveButton = UIButton(type: .system).then {
+        $0.setTitle("저장하기", for: .normal)
+        $0.backgroundColor = .primary
         $0.layer.cornerRadius = 8
+        $0.setTitleColor(.white, for: .normal)
+        $0.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        
+        $0.addTarget(self, action: #selector(saveBtnTapped), for: .touchUpInside)
     }
 
     // MARK: - LifeCycle
@@ -50,16 +54,6 @@ class EditInfoVC: UIViewController {
         
         configureUI()
     }
-//    // 네비게이션 바 가리기
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//        navigationController?.setNavigationBarHidden(true, animated: false)
-//    }
-//    // 다른 뷰로 이동할 때 다시 보이기
-//    override func viewWillDisappear(_ animated: Bool) {
-//        super.viewWillDisappear(animated)
-//        navigationController?.setNavigationBarHidden(false, animated: false)
-//    }
     
     // MARK: - Configure UI
     private func configureUI() {
@@ -94,6 +88,12 @@ class EditInfoVC: UIViewController {
             $0.height.equalTo(50)
         }
     }
+    
+    // MARK: - Button Actions
+    @objc
+    private func saveBtnTapped() {
+        self.navigationController?.popViewController(animated: true) // pop으로 현재뷰 삭제되고 이전뷰로 이동
+    }
 }
 
 
@@ -101,7 +101,6 @@ class EditInfoVC: UIViewController {
 /*
  
  - 수정 취소 버튼?
- - 키보드 설정
- - 버튼 액션
+ - 수정완료 얼럿 띄우고 확인시 이동
  
 */
