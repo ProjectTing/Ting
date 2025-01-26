2366128e6848a4f3e929db1b2bf4d692c31066f7
youngjin <bryjna07@nate.com>
2025-01-26 18:43:53 +0900

    Cmt: TODO 주석추가

--- Ting/Post/PostMainVC.swift
+++ Ting/Post/PostMainVC.swift
@@ -19,6 +19,10 @@ final class PostMainVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpNaviBar()
+        /// TODO - 셀 : 메인뷰 셀 등록하기,
+        /// 셀 클릭 시 화면이동 -> 게시글 상세화면
+        /// 글쓰기 버튼 -> 게시글 작성뷰
+        /// 구인/구직 두개로 나눠야함, 파일명
     }
     
     // 네비바 생성
