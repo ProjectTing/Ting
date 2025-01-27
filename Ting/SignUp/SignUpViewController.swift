8a0c060e273596a37f090e770878211916905d63
solnamul <sol7113@gmail.com>
2025-01-27 14:24:58 +0900

    Feat: Hex 코드 삭제

--- Ting/SignUp/SignUpViewController.swift
+++ Ting/SignUp/SignUpViewController.swift
@@ -65,20 +65,3 @@ extension SignUpViewController: ASAuthorizationControllerDelegate {
         print("Apple 로그인 실패: \(error.localizedDescription)")
     }
 }
-
-// MARK: - UIColor HEX 코드 변환 확장
-extension UIColor {
-    convenience init(hex: String, alpha: CGFloat = 1.0) {
-        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
-        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
-        
-        var rgb: UInt64 = 0
-        Scanner(string: hexSanitized).scanHexInt64(&rgb)
-        
-        let red = CGFloat((rgb >> 16) & 0xFF) / 255.0
-        let green = CGFloat((rgb >> 8) & 0xFF) / 255.0
-        let blue = CGFloat(rgb & 0xFF) / 255.0
-        
-        self.init(red: red, green: green, blue: blue, alpha: alpha)
-    }
-}
