d14dab8e858cbfcccc22a375b66663c30ccb86cb
Quaker-Lee <tkacl2010@gmail.com>
2025-02-11 20:54:49 +0900

    Refactor: UserDefaults 저장코드 삭제, 간격 수정

--- Ting/MyPage/AddUserInfo/AddUserInfoVC.swift
+++ Ting/MyPage/AddUserInfo/AddUserInfoVC.swift
@@ -135,7 +135,7 @@ class AddUserInfoVC: UIViewController, UITextFieldDelegate {
     }
     
     
-    // MARK: - Button Actions & Firebase에 업로드
+    // MARK: - Save Button Action & Firebase에 업로드
     @objc
     private func saveBtnTapped() {
         // MARK: 닉네임 중복 검사
@@ -150,53 +150,47 @@ class AddUserInfoVC: UIViewController, UITextFieldDelegate {
                 }
                 return
             }
-           
-           // userInfo 객체 생성
-           let userInfo = UserInfo(
-               userId: userId,
-               nickName: nickname,
-               role: roleField.textField.text ?? "",
-               techStack: techStackField.textField.text ?? "",
-               tool: toolField.textField.text ?? "",
-               workStyle: workStyleField.textField.text ?? "",
-               location: locationField.textField.text ?? "",
-               interest: interestField.textField.text ?? ""
-           )
-               
-           // textField가 다 채워졌는지 확인하기 위해 배열에 저장
-           let isAddInfoEmpty = [
-               userInfo.nickName,
-               userInfo.role,
-               userInfo.techStack,
-               userInfo.tool,
-               userInfo.workStyle,
-               userInfo.location,
-               userInfo.interest
-           ]
-           
-           // 텍스트 필드가 전부 채워졌는지 확인하고 서버에 업로드
-           if isAddInfoEmpty.allSatisfy({ !$0.isEmpty }) {
-               UserInfoService.shared.createUserInfo(info: userInfo) { result in
-                   switch result {
-                   case .success:
-                       // UserDefaults에 Id저장
-                       UserDefaults.standard.set(userInfo.userId, forKey: "userId")
-                       
-                       // 저장된 userId출력
-                       if let savedUserId = UserDefaults.standard.string(forKey: "userId") {
-                           print("저장된 userId: \(savedUserId)")
-                       }
-                       let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate
-                       sceneDelegate?.window?.rootViewController = TabBar()
-                       print("업로드 성공. | UserDefaults 저장 성공 | MainView로 이동함")
-                   case .failure(let error):
-                       print("업로드 실패: \(error)")
-                   }
-               }
-           } else {
-               basicAlert(title: "오류", message: "빈칸 없이 입력해주세요.")
-           }
-       }
+            
+            // MARK: - Firebase Create
+            // userInfo 객체 생성
+            let userInfo = UserInfo(
+                userId: userId,
+                nickName: nickNameField.textField.text ?? "",
+                role: roleField.textField.text ?? "",
+                techStack: techStackField.textField.text ?? "",
+                tool: toolField.textField.text ?? "",
+                workStyle: workStyleField.textField.text ?? "",
+                location: locationField.textField.text ?? "",
+                interest: interestField.textField.text ?? ""
+            )
+            
+            // textField가 다 채워졌는지 확인하기 위해 배열에 저장
+            let isAddInfoEmpty = [
+                userInfo.nickName,
+                userInfo.role,
+                userInfo.techStack,
+                userInfo.tool,
+                userInfo.workStyle,
+                userInfo.location,
+                userInfo.interest
+            ]
+            
+            // 텍스트 필드가 전부 채워졌는지 확인하고 서버에 업로드
+            if isAddInfoEmpty.allSatisfy({ !$0.isEmpty }) {
+                UserInfoService.shared.createUserInfo(info: userInfo) { result in
+                    switch result {
+                    case .success:
+                        let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate
+                        sceneDelegate?.window?.rootViewController = TabBar() // 메인화면으로 이동
+                        print("업로드 성공. | UserDefaults 저장 성공 | MainView로 이동함")
+                    case .failure(let error):
+                        print("업로드 실패: \(error)")
+                    }
+                }
+            } else {
+                basicAlert(title: "오류", message: "빈칸 없이 입력해주세요.")
+            }
+        }
     }
     
     // MARK: 키보드 설정
